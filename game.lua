25a13c2024a9047bc7da8b0a1123ff9692cf8fc8
Alexander Mann <amann@student.hive.fi>
2023-02-25 16:29:55 +0200

    Added a reload mechanic. There is a timer that scales based on the number of bullets below capacity at the time the reload button is pressed. Also, replaced snake_casing with camelCase, THIS IS NOT C

--- game.lua
+++ game.lua
@@ -2,26 +2,39 @@ local cron = require 'cron'
 local Game = {}
 
 function Game:load()
+	math.randomseed(os.time()) --random number seeding is required
+	
 	self.target = {}
 	self.target.radius = 50
-	math.randomseed(os.time()) --random number seeding is required
 	self.target.x, self.target.y = self:positionTarget()
 
 	self.score = 0
 	self.seconds = 0
-	self.ammo = 6
+	self.ammoCap = 6
+	self.ammo = self.ammoCap
+	self.reloadTime = 0
+	self.reloadDuration = 0.5
+	self.reloading = false
 	self.timer = cron.every(1, function() self.seconds = self.seconds + 1 end) 
-	self.initialtime = love.timer.getTime()
+	self.initialTime = love.timer.getTime()
 	self.font = love.graphics.newFont(40)
 end
 
 function Game:shoot( x, y, button)
-	if button == 1 and self.ammo > 0 then
+	if button == 1 and self.ammo > 0 and self.reloading == false then
 		if self:distanceBetween(x, y, self.target.x, self.target.y) <= self.target.radius then
 			self.score = self.score + 1
 			self.target.x, self.target.y = self:positionTarget()
 		end
 		self.ammo = self.ammo - 1
+	end
+end
+
+function Game:reload(key)
+	if key == "space" and self.reloading == false then
+		self.reloadTime = self.reloadDuration * (self.ammoCap - self.ammo)
+		self.ammo = self.ammoCap
+		self.reloading = true
 	end
 end
 
@@ -35,16 +48,26 @@ function Game:distanceBetween(mouseX, mouseY, targetX, targetY)
 	return math.sqrt( math.pow((mouseX - targetX), 2) + math.pow((mouseY - targetY), 2) )
 end
 
-function Game:update(dt)
-	self.timer:update(dt)
+function Game:decreaseReloadTime(dt)
+	if self.reloading then
+		self.reloadTime = self.reloadTime - dt
+		if self.reloadTime <= 0 then
+			self.reloading = false
+		end
+	end
 end
 
-function Game:draw_game_target()
+function Game:update(dt)
+	self.timer:update(dt)
+	self:decreaseReloadTime(dt)
+end
+
+function Game:drawGameTarget()
 	love.graphics.setColor(love.math.colorFromBytes(0, 153, 0))
 	love.graphics.circle("fill", self.target.x, self.target.y, self.target.radius)
 end
 
-function Game:draw_game_hud()
+function Game:drawGameHud()
 	love.graphics.setColor(1, 1, 1)
 	love.graphics.setFont(self.font)
 	love.graphics.print("Score:", 0, 0)
@@ -53,8 +76,8 @@ function Game:draw_game_hud()
 end
 
 function Game:draw()
-	self:draw_game_target()
-	self:draw_game_hud()
+	self:drawGameTarget()
+	self:drawGameHud()
 end
 
 return Game
--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
-local cron = require 'cron'
-
 local Game = require("game")
+
+-- MAIN FUNCTIONS --
 
 --called when the game starts. Handles all the setup stuff
 function love.load()
@@ -24,8 +24,20 @@ function love.draw()
 	end
 end
 
+-- KEY HANDLERS --
+
 function love.mousepressed( x, y, button, istouch, presses )
 	if game_state == 0 then
 		Game:shoot(x, y, button)
+	end
+end
+
+function love.keypressed(key)
+	if game_state == 0 then
+		Game:reload(key)
+	end
+	
+	if key == "escape" then --placeholder to close the game while testing
+		love.event.quit()
 	end
 end
